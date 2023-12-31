d864f6a5372cbc0637474a1fa21e22e3af7d90c4
DavidKirsch-DBC <153624310+DavidKirsch-DBC@users.noreply.github.com>
2023-12-31 16:30:33 -0300

    add: listagem boilerplate

--- back/src/listaAnuncios.java
+++ back/src/listaAnuncios.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class listaAnuncios {
+    List<detalhesPropriedade> propriedades = new ArrayList<detalhesPropriedade>();
+
+    public void listarAnuncios(detalhesPropriedade propriedade) {
+
+    }
+    public void adicionarAnuncios(detalhesPropriedade propriedade) {
+
+    }
+    public void removerAnuncios(detalhesPropriedade propriedade) {
+
+    }
 }
