ad9341a23a9b5a2b1eaa6070bd0c81975e20229c
Leonardo Daniel Rauber <leonardo.rauber@dbccompany.com.br>
2024-01-03 20:43:31 -0300

    feat(Usuario):Inclusão da classe usuario e suas classes filhas(Locador e Locatario)

---
+++ Back/src/Locador.java
@@ -0,0 +1,7 @@
+public class Locador extends Usuario{
+
+
+    public Locador(String nomeUsuario, String email, String senha, String nome, String nascimento) {
+        super(nomeUsuario, email, senha, nome, nascimento);
+    }
+}
---
+++ Back/src/Locatario.java
@@ -0,0 +1,6 @@
+public class Locatario extends Usuario {
+
+    public Locatario(String nomeUsuario, String email, String senha, String nome, String nascimento) {
+        super(nomeUsuario, email, senha, nome, nascimento);
+    }
+}
