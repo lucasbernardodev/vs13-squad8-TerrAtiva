2a89d5abe439c57cd72e3b7f863494e4aacc17ca
leonardo.rauber <leo_rauber_@hotmail.com>
2024-01-04 19:38:46 -0300

    feat(ValoresRandomicos):Adiciona limite de valor aos randomicos

--- Back/src/util/ValoresRandomicos.java
+++ Back/src/util/ValoresRandomicos.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class ValoresRandomicos {
     static Random geradorRandomico = new Random();
     public static int gerarValorPositivo(){
-        int inteiroAleatorio = geradorRandomico.nextInt();
+        int inteiroAleatorio = geradorRandomico.nextInt(999);
 
         while (inteiroAleatorio < 0) {
-            inteiroAleatorio = geradorRandomico.nextInt();
+            inteiroAleatorio = geradorRandomico.nextInt(999);
         }
 
         return  inteiroAleatorio;
