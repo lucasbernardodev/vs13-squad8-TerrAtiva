b3809493733ddd1327fc2f71b480edce25ba6b31
Italo Lacerda <italojose917@gmail.com>
2024-01-04 18:47:54 -0300

    feat(Locador): Sobreescreve metodo ToString

--- Back/src/models/Locador.java
+++ Back/src/models/Locador.java
@@ -3,7 +3,24 @@ package models;
 public class Locador extends Usuario{
 
 
-    public Locador(String nomeUsuario, String email, String senha, String nome, String nascimento) {
+    public Locador(
+            String nomeUsuario,
+            String email,
+            String senha,
+            String nome,
+            String nascimento
+    ) {
         super(nomeUsuario, email, senha, nome, nascimento);
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + " Locador{" + "\n" +
+                "   id: " + this.getId() + "," + "\n" +
+                "   nomeUsuario: " + this.getNomeUsuario() + "," + "\n" +
+                "   email: " + this.getEmail() + "," + "\n" +
+                "   nome: " + this.getNome() + "," + "\n" +
+                "   dataDeNascimento: "  + this.getNascimento() + "\n" +
+                " };" + "\n";
     }
 }
