719edec8c2252416704ce59b3cbcc30229296f93
shaienne23 <shaienneo@gmail.com>
2024-01-03 22:02:58 -0300

    feat(Terreno): Inclusão da classe Terreno

---
+++ Back/src/Terreno.java
@@ -0,0 +1,86 @@
+public class Terreno {
+    private int id;
+    private String titulo;
+    private String descricao;
+    private String localizacao;
+    private String tamanho;
+    private double preco;
+    private Locatario proprietario;
+    private Locador locador;
+    private boolean disponivel;
+
+   public Terreno(int id, String titulo, String descricao, String localizacao,
+                  String tamanho, double preco, Locatario proprietario,
+                  Locador locador, boolean disponivel){
+            this.id = id;
+            this.titulo = titulo;
+            this.descricao = descricao;
+            this.localizacao = localizacao;
+            this.tamanho = tamanho;
+            this.preco = preco;
+            this.proprietario = proprietario;
+            this.locador = locador;
+            this.disponivel = disponivel;
+   }
+
+   public int getId(){
+       return id;
+   }
+   public void setId(int id){
+       this.id = id;
+   }
+   public String getTitulo(){
+       return titulo;
+   }
+   public void setTitulo(String titulo){
+       this.titulo = titulo;
+   }
+   public String getDescricao(){
+       return descricao;
+   }
+   public void setDescricao( String descricao){
+       this.descricao = descricao;
+   }
+   public String getLocalizacao(){
+       return localizacao;
+   }
+    public void setLocalizacao(String localizacao){
+       this.localizacao = localizacao;
+    }
+    public String getTamanho(){
+       return tamanho;
+    }
+    public void setTamanho(String tamanho){
+       this.tamanho = tamanho;
+    }
+    public double getPreco(){
+       return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Locatario getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(Locatario proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    public Locador getLocador() {
+        return locador;
+    }
+
+    public void setLocador(Locador locador) {
+        this.locador = locador;
+    }
+
+    public boolean getDisponivel(){
+       return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+   }
+}
