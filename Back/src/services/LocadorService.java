a3f711a4ba5e9af07df325f91da8c6a44c95b566
Italo Lacerda <italojose917@gmail.com>
2024-01-04 18:48:46 -0300

    feat(LocadorService): Implementa metodos cadastrar e atualizarPerfil

--- Back/src/services/LocadorService.java
+++ Back/src/services/LocadorService.java
@@ -1,23 +1,48 @@
 package services;
 
+import database.BancoDeDados;
 import interfaces.CrudConta;
 import models.Locador;
 import models.Terreno;
+import util.ValoresRandomicos;
 
 import java.util.List;
 
 public class LocadorService implements CrudConta {
     //TODO: Lógica dos métodos (faltava o banco de dados no momento pra fazer)
     //TODO: Jogar em pasta de services
+    public void cadastrar(Locador novoUsuario) {
 
-    public final void cadastrar(Locador novoUsuario) {
+        novoUsuario.setId(ValoresRandomicos.gerarValorPositivo());
+
+        BancoDeDados.locadoresDataBase.add(novoUsuario);
 
     }
-    public final boolean atualizarPerfil(String nomeUsuario, String email,
-                                         String senha, String nome, String nascimento)
+    public final boolean atualizarPerfil(int id, String nomeUsuario, String email,
+                                         String nome, String nascimento)
     {
+        List<Locador> listaDeLocadoresDb = BancoDeDados.locadoresDataBase;
+
+        Locador perfilAtual = listaDeLocadoresDb
+                .stream()
+                .filter(locador -> locador.getId() == id)
+                .toList()
+                .get(0);
+
+        perfilAtual.setNomeUsuario(nomeUsuario);
+        perfilAtual.setEmail(email);
+        perfilAtual.setNome(nome);
+        perfilAtual.setNascimento(nascimento);
+
+
        return true;
+    };
+
+    @Override
+    public boolean atualizarPerfil(String nomeUsuario, String email, String senha, String nome, String nascimento) {
+        return false;
     }
+
     public final void deletarPerfil(int id) {
 
     }
