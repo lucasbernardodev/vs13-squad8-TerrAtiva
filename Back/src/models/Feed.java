c7b03f225bb76b37bf6d483255fc4f04357c4a9e
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2024-01-04 21:47:43 -0300

    feat(Feed): finalizando implementação dos métodos

--- Back/src/models/Feed.java
+++ Back/src/models/Feed.java
@@ -1,11 +1,13 @@
 package models;
 
 import database.BancoDeDados;
+
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Feed {
-
     public List<Terreno> mostrarTerrenosDisponveis(){
         ArrayList<Terreno> terrenosDisponiveis= new ArrayList<Terreno>();
 
@@ -16,6 +18,39 @@ public class Feed {
         }
         return terrenosDisponiveis;
     }
-    
-    
+
+    public List<Terreno> mostrarTerrenosPorLocalizacao(String localizacao){
+        return BancoDeDados.terrenosDataBase
+                .stream()
+                .filter(terreno -> terreno.getLocalizacao().equals(localizacao))
+                .collect(Collectors.toList());
+    }
+
+    public List<Terreno> mostrarTerrenosPorPrecoMenor(double valor){
+        return BancoDeDados.terrenosDataBase
+                .stream()
+                .filter(terreno -> terreno.getPreco() <= valor)
+                .collect(Collectors.toList());
+    }
+    public Terreno mostrarTerrenosPorTitulo(String titulo){
+        return BancoDeDados.terrenosDataBase
+                .stream()
+                .filter(terreno -> terreno.getTitulo().toLowerCase().equals(titulo.toLowerCase()))
+                .findFirst()
+                .get();
+    }
+
+    public List<Terreno> mostrarTerrenosPorLocatario(String nomeLocatario) {
+        return BancoDeDados.terrenosDataBase
+                .stream()
+                .filter(terreno -> terreno.getProprietario().getNome().equals(nomeLocatario))
+                .collect(Collectors.toList());
+    }
+
+
+
+
+
+
+
 }
