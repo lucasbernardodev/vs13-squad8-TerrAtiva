f8f6193827d0c6ceb7f3040e262652e397d267d6
Italo Lacerda <italojose917@gmail.com>
2024-01-07 11:51:00 -0300

    feat(Main):  Adiciona classe App com método 'iniciaAplicacao' e atualiza ponto de entrada no main

--- Back/src/Main.java
+++ Back/src/Main.java
@@ -1,4 +1,6 @@
+import app.App;
 import app.Feed;
+import app.menus.SessaoMenu;
 import database.DadosMocados;
 import app.Menus;
 
@@ -6,12 +8,13 @@ import app.Menus;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Bem vindo a TerrAtiva!" +
-                "\nA platforma que facilita o arrendamento de terras, conectando pessoas!");
-
-        DadosMocados.databaseOn();
-        Menus.menuTemp();
-        Feed app = new Feed();
+//        System.out.println("Bem vindo a TerrAtiva!" +
+//                "\nA platforma que facilita o arrendamento de terras, conectando pessoas!");
+//
+//        DadosMocados.databaseOn();
+//        Menus.menuTemp();
+//        Feed app = new Feed();
+        App.iniciaAplicacao();
 
 
     }
