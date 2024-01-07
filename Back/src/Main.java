5964901163341b5c580a99f0f9ec773f1bc68e8e
Italo Lacerda <italojose917@gmail.com>
2024-01-07 11:54:19 -0300

    refactor(Main): Remove comentários e códigos desnecessários

--- Back/src/Main.java
+++ Back/src/Main.java
@@ -1,21 +1,7 @@
 import app.App;
-import app.Feed;
-import app.menus.SessaoMenu;
-import database.DadosMocados;
-import app.Menus;
-
 
 public class Main {
     public static void main(String[] args) {
-
-//        System.out.println("Bem vindo a TerrAtiva!" +
-//                "\nA platforma que facilita o arrendamento de terras, conectando pessoas!");
-//
-//        DadosMocados.databaseOn();
-//        Menus.menuTemp();
-//        Feed app = new Feed();
         App.iniciaAplicacao();
-
-
     }
 }
