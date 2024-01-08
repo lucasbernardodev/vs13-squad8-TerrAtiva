565169b127579d3b1559785bd7e893dc2cf5fd2b
leonardo.rauber <leo_rauber_@hotmail.com>
2024-01-08 00:37:09 -0300

    refactor(Validação)/Correção

--- Back/src/util/Validacao.java
+++ Back/src/util/Validacao.java
@@ -76,7 +76,6 @@ public class Validacao {
 
         if (nomeUsuario.isBlank()) throw new EmptyDataException("Seu NOME DE USUÁRIO não pode estar vazio!");
         if (email.isBlank()) throw new EmptyDataException("Seu EMAIL não pode estar vazio!");
-        if (senha.isBlank()) throw new EmptyDataException("Sua SENHA não pode estar vazio!");
         if (nome.isBlank()) throw new EmptyDataException("Seu NOME não pode estar vazio!");
         if (nascimento.isBlank()) throw new EmptyDataException("Sua DATA DE NASCIMENTO não pode estar vazio!");
     }
