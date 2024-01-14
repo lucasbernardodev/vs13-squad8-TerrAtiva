2c1579abdf83aeb8aeb4affc4e88163308feac14
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2024-01-14 11:39:51 -0300

    refactor(repository-feed): apenas imports utilizados

--- Back/src/repository/FeedRepository.java
+++ Back/src/repository/FeedRepository.java
@@ -4,8 +4,12 @@ import database.BancoDeDados;
 import infra.exceptions.DbException;
 import models.Terreno;
 
-import java.sql.*;
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class FeedRepository {
     Connection connection;
