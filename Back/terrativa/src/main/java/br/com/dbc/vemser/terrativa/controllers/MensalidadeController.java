912f2db97d01aaf8643cdf434b50d4d2c4c68aa8
leonardo.rauber <leo_rauber_@hotmail.com>
2024-01-22 11:59:46 -0300

    feat:Ajuste da MensalidadeController.

--- Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/controllers/MensalidadeController.java
+++ Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/controllers/MensalidadeController.java
@@ -1,26 +1,48 @@
 package br.com.dbc.vemser.terrativa.controllers;
 
+import br.com.dbc.vemser.terrativa.dto.RequestMensalidade;
+import br.com.dbc.vemser.terrativa.dto.ResponseMensalidade;
+import br.com.dbc.vemser.terrativa.services.ContratoService;
 import br.com.dbc.vemser.terrativa.services.MensalidadeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
+@RequestMapping("/mensalidade") // localhost:8081/mensalidade
+@Slf4j
 public class MensalidadeController {
 
-    public final MensalidadeService mensalidadeService;
+    private final MensalidadeService mensalidadeService;
 
-    public String atualizarMensalidade(Integer mensalidadeID, Integer contratoID, Double valorMensal, Integer anoExercicio) {
-            mensalidadeService.alterarMensalidade(mensalidadeID, contratoID, valorMensal, anoExercicio);
-            return "Mensalidade Atualizada com Sucesso!";
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseMensalidade> atualizarMensalidade(@PathVariable("id") @NotNull Integer id, @RequestBody @Valid RequestMensalidade requestMensalidade) throws Exception{
+        log.info("Atualizando mensalidade");
+        ResponseMensalidade responseMensalidade = mensalidadeService.alterarMensalidade(id, requestMensalidade);
+        log.info("Mensalidade atualizada");
+        return new ResponseEntity<>(responseMensalidade, HttpStatus.OK);
+
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseMensalidade> resgatarEnderecoPorID(@PathVariable Integer id) throws Exception{
+        return new ResponseEntity<>(mensalidadeService.resgatarMensalidadePorId(id), HttpStatus.OK);
     }
 
-    public String resgatarEnderecoPorID(Integer id){
-            return mensalidadeService.resgatarMensalidadePorId(id).toString();
-    }
-
-    public String deletarMensalidade(Integer id) {
-            mensalidadeService.deletarMensalidade(id);
-            return "Mensalidade deletada com Sucesso";
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarMensalidade(@PathVariable Integer id) throws Exception {
+        log.info("Deletando mensalidade");
+        mensalidadeService.deletarMensalidade(id);
+        log.info("Mensalidade deletada");
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
