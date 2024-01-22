72d5b0ed93d8ae69c6533db9a2f176f6b14a64a2
leonardo.rauber <leo_rauber_@hotmail.com>
2024-01-22 12:01:51 -0300

    feat:Ajuste ResquestMensalidade com validações.

--- Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/dto/RequestMensalidade.java
+++ Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/dto/RequestMensalidade.java
@@ -1,9 +1,10 @@
 package br.com.dbc.vemser.terrativa.dto;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 
 @AllArgsConstructor
@@ -12,9 +13,12 @@ import java.time.LocalDate;
 public class RequestMensalidade {
 
     private Integer mensalidadeID;
+    @NotNull
     private Integer contratoID;
+    @NotNull
+    @Positive
     private double valorMensal;
+    @NotNull
     private Integer anoExercicio;
-    private LocalDate dataReajuste;
 
 }
