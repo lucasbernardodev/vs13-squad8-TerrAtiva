80e2d60d2c6058c424a78b0615fcd9a2ea96f10c
Renan Meira <122182644+RenanStMeira@users.noreply.github.com>
2024-01-24 10:57:04 -0300

    refaturando feed

--- Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/controllers/documentacao/IFeedController.java
+++ Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/controllers/documentacao/IFeedController.java
@@ -1,4 +1,17 @@
 package br.com.dbc.vemser.terrativa.controllers.documentacao;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 public interface IFeedController {
+    @Operation(summary = "Listar Aluguel", description = "Lista todas os alugueis do banco")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna a lista de alugueis"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+
 }
