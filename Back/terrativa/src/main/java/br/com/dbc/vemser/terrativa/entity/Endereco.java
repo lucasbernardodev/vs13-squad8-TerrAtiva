d8dcbd2db0648169cb9a7f862421647df0e6303f
Renan Meira <122182644+RenanStMeira@users.noreply.github.com>
2024-02-02 13:50:36 -0300

    refaturando terreno para jpa

--- Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/entity/Endereco.java
+++ Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/entity/Endereco.java
@@ -4,18 +4,39 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity(name = "ENDERECOS")
 public class Endereco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_ENDERECO")
+    @SequenceGenerator(name = "SEQ_ENDERECO", sequenceName = "SEQ_ENDERECO", allocationSize = 1)
+    @Column(name = "ENDERECO_ID")
     private Integer id;
+
+    @Column(name = "usuario_id")
     private Integer usuarioID;
+
+    @Column(name = "logradouro")
     private String logradouro;
+
+    @Column(name = "numero")
     private Integer numero;
+
+    @Column(name = "complemento")
     private String complemento;
+
+    @Column(name = "bairro")
     private String bairro;
+
+    @Column(name = "municipio_cod_ibge")
     private Integer codigoMunicipioIBGE;
+
+    @Column(name = "cep")
     private Integer cep;
 
 }
