55f755d255a22d5b08b0a10037dfe5d1c5825569
leonardo.rauber <leo_rauber_@hotmail.com>
2024-02-02 12:26:20 -0300

    feat:Alteração das classes de Contrato para o método JPA.

--- Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/dto/requests/RequestContratoCreateDTO.java
+++ Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/dto/requests/RequestContratoCreateDTO.java
@@ -20,7 +20,7 @@ public class RequestContratoCreateDTO {
     @Schema(description = "Identificador único do Contrato.", example = "1")
     private Integer id;
 
-@   NotNull(message = "O ID do locatário não pode ser nulo")
+    @NotNull(message = "O ID do locatário não pode ser nulo")
     @Schema(description = "Identificador único do Locatário.", example = "1")
     private Integer locatarioID;
 
--- Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/entity/Contrato.java
+++ Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/entity/Contrato.java
@@ -1,22 +1,48 @@
 package br.com.dbc.vemser.terrativa.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Setter
+@Getter
+@ToString
+@Entity(name = "CONTRATOS")
 public class Contrato {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_CONTRATO")
+    @SequenceGenerator(name = "SEQ_CONTRATO", sequenceName = "SEQ_CONTRATO", allocationSize = 1)
+    @Column(name = "CONTRATO_ID")
     private Integer id;
+
+    @Column(name = "LOCATARIO_ID")
     private Integer locatarioID;
+
+    @Column(name = "TERRENO_ID")
     private Integer terrenoID;
+
+    @Column(name = "ATIVO")
     private String ativo;
+
+    @Column(name = "DATA_ASSINATURA")
     private LocalDate dataAssinatura;
+
+    @Column(name = "DATA_INICIO")
     private LocalDate dataInicio;
+
+    @Column(name = "DATA_FINAL")
     private LocalDate dataFinal;
+
+    @Column(name = "DIA_VENCIMENTO_ALUGUEL")
     private Integer dataVencimentoAluguel;
+
+    @Column(name = "CRIADO")
+    private String criado;
+
+    @Column(name = "EDITADO")
+    private String editado;
 }
