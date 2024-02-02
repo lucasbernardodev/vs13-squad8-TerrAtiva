2ee3fd97185e3adca32a88647b28e1c5f7e35327
leonardo.rauber <leo_rauber_@hotmail.com>
2024-02-02 12:27:06 -0300

    feat:Alteração da classe de ContratoService para JPA e inclusão do método de create na própria classe.

--- Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/services/ContratoService.java
+++ Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/services/ContratoService.java
@@ -8,30 +8,49 @@ import br.com.dbc.vemser.terrativa.repository.ContratoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class ContratoService {
+
     private final ContratoRepository contratoRepository;
 
     public ResponseContratoDTO resgatarContratoPorId(Integer id) {
-        Contrato contrato = contratoRepository.resgatarDadosPorId(id);
+        Contrato contrato = contratoRepository.findById(id).get();
         return ContratoMapper.contratoParaResponseContrato(contrato);
     }
 
-    public ResponseContratoDTO alterar(RequestContratoCreateDTO contrato)throws Exception {
-    return ContratoMapper.contratoParaResponseContrato(
-            contratoRepository.alterar(
-                    ContratoMapper.requestContratoParaContrato(contrato)));
+    public ResponseContratoDTO createContrato(RequestContratoCreateDTO contratCrate){
+        Contrato contrato = ContratoMapper.requestContratoParaContrato(contratCrate);
+        Contrato contratSalvo = contratoRepository.save(contrato);
+        return ContratoMapper.contratoParaResponseContrato(contrato);
+
     }
 
-    public Integer getNextId() {
-        return contratoRepository.getNextId();
+    public ResponseContratoDTO alterar(Integer id, RequestContratoCreateDTO contratoCreate)throws Exception {
+        Contrato contrato = findByID(id);
+        contrato.setLocatarioID(contratoCreate.getLocatarioID());
+        contrato.setDataAssinatura(contratoCreate.getDataAssinatura());
+        contrato.setDataInicio(contratoCreate.getDataInicio());
+        contrato.setDataFinal(contratoCreate.getDataFinal());
+        contrato.setDataVencimentoAluguel(contratoCreate.getDataVencimentoAluguel());
+        contrato.setEditado(Instant.now().toString());
+        contratoRepository.save(contrato);
+        ResponseContratoDTO responseContratoDTO = ContratoMapper.contratoParaResponseContrato(contrato);
+        return responseContratoDTO;
     }
 
     public void deletar(Integer id) {
-        contratoRepository.deletar(id);
+        Contrato contrato = findByID(id);
+        contrato.setAtivo("N");
+        contratoRepository.save(contrato);
     }
 
+    private Contrato findByID(Integer id){
+        return contratoRepository.findById(id).get();
+    }
 
 }
