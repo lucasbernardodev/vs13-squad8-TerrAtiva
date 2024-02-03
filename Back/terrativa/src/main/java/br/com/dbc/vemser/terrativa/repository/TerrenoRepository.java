397a78f5b0368e90068af99e41d30f3b563c26f4
Renan Meira <122182644+RenanStMeira@users.noreply.github.com>
2024-02-03 17:04:56 -0300

    refactor feed

--- Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/repository/TerrenoRepository.java
+++ Back/terrativa/src/main/java/br/com/dbc/vemser/terrativa/repository/TerrenoRepository.java
@@ -15,33 +15,19 @@ public interface TerrenoRepository extends JpaRepository<Terreno, Integer> {
 
     List<Terreno> findAllByDisponivelEqualsAndProprietarioID(String disponivel, Integer donoID);
 
-
-
     @Query("""
-        SELECT new br.com.dbc.vemser.terrativa.dto.reponses.ResponseFeedQuantidadeAnunciosDTO(
-            ESTADO_MUNICIPIOS.nomeEstado,
-            COUNT(*),
-            ESTADO_MUNICIPIOS.estadoCod
-        )
-        FROM TERRENOS t
-        JOIN ENDERECO_TERRENOS et ON t.enderecoID = et.id
-        JOIN ESTADO_MUNICIPIOS em ON et.codMunicipioIBGE = em.municipioCodIBGE
-        WHERE t.disponivel = 'S'
-        GROUP BY em.nomeEstado , em.estadoCod
-        ORDER BY em.nomeEstado
-    """)
+    SELECT new br.com.dbc.vemser.terrativa.dto.reponses.ResponseFeedQuantidadeAnunciosDTO(
+        em.nomeEstado,
+        COUNT(*),
+        em.estadoCod
+    )
+    FROM TERRENOS t
+    JOIN ENDERECO_TERRENOS et ON t.enderecoID = et.id
+    JOIN ESTADO_MUNICIPIOS em ON et.codMunicipioIBGE = em.municipioCodIBGE
+    WHERE t.disponivel = 'S'
+    GROUP BY em.nomeEstado, em.estadoCod
+    ORDER BY em.nomeEstado
+""")
     List<ResponseFeedQuantidadeAnunciosDTO> quantidadeAnuncios();
 
-
-
-//    @Query(value = """
-//    SELECT NOME_ESTADO as nomeEstado, COUNT(*) as quantidadeAnuncios, em.ESTADO_COD as codigoEstado
-//    FROM TERRENOS t
-//    JOIN ENDERECO_TERRENOS et ON t.ENDERECO_TERRENO_ID = et.ENDERECO_TERRENO_ID
-//    JOIN ESTADO_MUNICIPIOS em ON et.MUNICIPIO_COD_IBGE = em.MUNICIPIO_COD_IBGE
-//    WHERE t.DISPONIVEL = 'S'
-//    GROUP BY NOME_ESTADO, em.ESTADO_COD
-//    ORDER BY NOME_ESTADO
-//""", nativeQuery = true)
-//    List<ResponseFeedQuantidadeAnunciosDTO> quantidadeAnuncios();
 }
